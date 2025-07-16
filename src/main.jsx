d0d794a7352aad2cc911a75a54b3f58fc3d3c000
EdrickMuthamia <edrickmuthamia05@gmail.com>
2025-07-16 10:06:45 +0300

    refactor: reorganize imports and update rendered components in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import Buttons from "./Events/Btns";
+import Form from "./Events/Form";
+
 import ArrayData from "./DisplayData/ArrayData";
 
 import PrimitiveData from "./Props/PrimitiveData";
@@ -8,21 +11,17 @@ import PrimitiveData from "./Props/PrimitiveData";
 import MiniProject from "./MiniProject";
 
 import "./css.css";
-<<<<<<< HEAD
-// import ".mycss/main.css";
-import Buttons from "./Events/Btns"
-import MyForm from "./Events/Form";
-import SimpleState from "./Hooks/State/SimpleState";  
-=======
 
 import "./mycss/main.css";
+import SimpleState from "./Hooks/State/SimpleState";
 
->>>>>>> 300c44529055f712cfa40ece2af3127c8ac614c7
+import SimpleState2 from "./Hooks/State/SimpleState2";
+
+import FormState from "./Hooks/State/FormState";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MiniProject />
-    <Buttons />
-    <MyForm />
-    <SimpleState />
+    <FormState />
   </StrictMode>
 );
+
