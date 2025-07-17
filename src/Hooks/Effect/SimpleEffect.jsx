7ac19ec20de85787ff5fbe50a4813557935f1f03
Arthur-codeX <daniel.karanja239@gmail.com>
2025-07-17 09:48:29 +0300

    updated effecet rerender

--- src/Hooks/Effect/SimpleEffect.jsx
+++ src/Hooks/Effect/SimpleEffect.jsx
@@ -1,6 +1,6 @@
 // ANGULAR-> LIFE CYCLE HOOK
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function SimpleEffect() {
   // useEffect(function,[dependency])
@@ -8,6 +8,11 @@ function SimpleEffect() {
   // useEffect(function,[])-> it will run only once
   // useEffect(function,[radius])-> initially when component is mounte and when radius
 
+  const [n, setN] = useState(0);
+
+  // Whenever state change occurs
+  // It triggures a rerender of the component
+  // if the useEffect does not have dependency it will rerender
   useEffect(() => {
     console.log("useEffect has run");
   });
@@ -15,6 +20,23 @@ function SimpleEffect() {
   return (
     <div>
       <h1>Simple Effect</h1>
+      <div>
+        <button
+          onClick={() => {
+            setN(n - 1);
+          }}
+        >
+          -
+        </button>
+        <span>{n}</span>
+        <button
+          onClick={() => {
+            setN(n + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
     </div>
   );
 }
